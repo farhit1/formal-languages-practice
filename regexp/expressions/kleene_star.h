74583e16460c623d8506923a2a59e659b841658f
farhit1 <farhit1@gmail.com>
2018-11-06 04:02:10 +0300

    added public

--- regexp/expressions/kleene_star.h
+++ regexp/expressions/kleene_star.h
@@ -7,6 +7,7 @@ namespace regexp {
 namespace expressions {
 
 class KleeneStar : public Expression, public Unary {
+public:
     using Unary::Unary;
 
     virtual Positions match(const std::string &s, size_t start) const override;
